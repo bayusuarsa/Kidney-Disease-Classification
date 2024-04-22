3c263b4b62868afd1b34bc220fca0bcd349a67ff
bayusuarsa <bayu.suarsa99@gmail.com>
2024-04-22 20:55:07 +0800

    model training added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -21,6 +24,18 @@ try:
     logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\n x===============x")
+except Exception as e:
+    logger.exception(e)
+
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f'**********************')
+    logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<")
+    model_trained = ModelTrainingPipeline()
+    model_trained.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\n x===============x")
 except Exception as e:
     logger.exception(e)
